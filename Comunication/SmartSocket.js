359ad524ce97d8a2748481702add8f972e5c1076
Pascal Hartmann <pascal-hartmann@live.de>
2019-03-24 19:11:16 +0100

    fixed errors when socket can not connect

--- Comunication/SmartSocket.js
+++ Comunication/SmartSocket.js
@@ -35,8 +35,15 @@ class SmartSocket {
             }
         };
         this.handler = inMessageHandler;
+
         return new Promise((resolve, reject) => {
+
             this.connection = Tls.connect(socketOptions, () => {
+                this.connection.on('error', (err) => {
+                    setImmediate(() => {
+                        this.handler.disconnectHandler();
+                    });
+                });
                 if (!this.connection.authorized) {
                     reject('Connection not authorized');
                 } else {
@@ -44,10 +51,9 @@ class SmartSocket {
                     this.setupSocketEvents();
                     resolve();
                 }
-            });
-            this.connection.on('apiError', (err) => {
+            })
+                .on('error', (err) => {
                 reject(err);
-                this.logService.error(err);
             });
         });
     }
@@ -69,11 +75,6 @@ class SmartSocket {
             setTimeout(() => {
                 self.renewSocket()
             }, 0);
-        });
-        this.connection.on('apiError', (err) => {
-            setImmediate(() => {
-                this.handler.disconnectHandler();
-            });
         });
         this.connection.on('end', () => {
             setImmediate(() => {
